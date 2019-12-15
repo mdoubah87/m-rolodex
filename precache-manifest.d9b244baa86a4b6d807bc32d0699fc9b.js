9a3ed9d77f97955e7351ca41a70594b8e78ecf64
mdoubah87 <mdoubah1410@gmail.com>
2019-12-15 16:44:06 +0000

    Updates

---
+++ precache-manifest.d9b244baa86a4b6d807bc32d0699fc9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705c285662fd8a54766f7c1a94d7b049",
+    "url": "/m-rolodex/index.html"
+  },
+  {
+    "revision": "bdfd739da4afab9372b2",
+    "url": "/m-rolodex/static/css/main.d833c1ce.chunk.css"
+  },
+  {
+    "revision": "189457582da94c4d093d",
+    "url": "/m-rolodex/static/js/2.585d026e.chunk.js"
+  },
+  {
+    "revision": "bdfd739da4afab9372b2",
+    "url": "/m-rolodex/static/js/main.6199c24c.chunk.js"
+  },
+  {
+    "revision": "9e38915349d6593af6ed",
+    "url": "/m-rolodex/static/js/runtime-main.ef4963e3.js"
+  }
+]);
